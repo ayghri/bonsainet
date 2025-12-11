c28d96c26af1d763a130a9da7ad6c11d7441d2e8
Ayoub G. <git@ayghri.com>
2025-12-11 15:12:34 -0700

    adding no_grad_forward

--- tests/benchmark_attn.py
+++ tests/benchmark_attn.py
@@ -88,7 +88,6 @@ def benchmark_sdpa_kernel():
                     backward_times += end - start
                     torch.cuda.empty_cache()
 
-
                 torch.cuda.synchronize()
 
                 for p in [keys, values, queries]:
@@ -109,7 +108,61 @@ def benchmark_sdpa_kernel():
 
                 torch.cuda.empty_cache()
                 print("-" * 25)
-    print("=" * 50)
+        print("=" * 50)
+
+    for name, backend in [
+        ("flash_attention", SDPBackend.FLASH_ATTENTION),
+        ("efficient_attention", SDPBackend.EFFICIENT_ATTENTION),
+        ("math", SDPBackend.MATH),
+    ]:
+        for is_causal in [False, True]:
+            print(f"Benchmarking {name} with is_causal={is_causal}")
+
+            torch.cuda.synchronize()
+
+            start_mem = torch.cuda.memory_allocated()
+            forward_times = 0
+            forward_mems = []
+
+            with sdpa_kernel(backend):
+                for _ in range(n_iters):
+                    torch.cuda.empty_cache()
+                    start_mem = torch.cuda.memory_allocated()
+                    torch.cuda.synchronize()
+                    start = time.time()
+                    with torch.no_grad():
+                        out_kernel = scaled_dot_product_attention(
+                            queries,
+                            keys,
+                            values,
+                            attn_mask=None,
+                            dropout_p=0.0,
+                            is_causal=is_causal,
+                        )
+                    torch.cuda.synchronize()
+                    end = time.time()
+
+                    end_mem = torch.cuda.memory_allocated()
+                    forward_times += end - start
+                    forward_mems.append(end_mem - start_mem)
+
+                    torch.cuda.empty_cache()
+
+                torch.cuda.synchronize()
+
+                for p in [keys, values, queries]:
+                    p.grad = None
+
+                print(
+                    f"{name} forward time: {forward_times / n_iters:.4f} seconds per iteration"
+                )
+                print(
+                    f"{name} forward memory usage: {np.max(forward_mems) / (1024**2):.2f} MB"
+                )
+
+                torch.cuda.empty_cache()
+                print("-" * 25)
+        print("=" * 50)
 
 
 if __name__ == "__main__":
